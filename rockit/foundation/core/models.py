2e2f137801a9d5511a15206224d662f32f978226
acreations@aeris <aaron.wong.home@gmail.com>
2014-01-28 23:16:33 +0100

    Added association model

--- rockit/foundation/core/models.py
+++ rockit/foundation/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Association(models.Model):
+  '''
+  Association information that exist in rockit network
+  '''
+  name = models.CharField(max_length=200, default='undefined')
+  description = models.CharField(max_length=500, blank=True)
+  namespace   = models.CharField(max_length=100)
+  entry       = models.CharField(max_length=100)
+  addable     = models.BooleanField(default=False)
+  date_added  = models.DateTimeField(auto_now_add=True, blank=True)
