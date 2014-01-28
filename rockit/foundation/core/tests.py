232b33d05531b8159b9c1d5dd82059420fb2bfb6
acreations@aeris <aaron.wong.home@gmail.com>
2014-01-28 23:50:38 +0100

    Added test for association

--- rockit/foundation/core/tests.py
+++ rockit/foundation/core/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from rockit.foundation.core.models import Association
+
+class AssociationTestCase(TestCase):
+
+	def setUp(self):
+		Association.objects.create(name='a', description='rockit description')
+
+	def test_it_should_not_be_addable_by_default(self):
+		"""An association should not be addable by default"""
+		association = Association.objects.get(name='a')
+		self.assertEqual(association.addable, False)
