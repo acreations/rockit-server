b9ca410c2f4c4ceb4625b4fc70178269770a2ee8
acreations@aeris <aaron.wong.home@gmail.com>
2014-01-28 22:34:52 +0100

    Changed name of the sqlite3 db

--- rockit/settings.py
+++ rockit/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+DB_NAME = "rockit.sqlite3"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -59,7 +60,7 @@ WSGI_APPLICATION = 'rockit.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, DB_NAME),
     }
 }
 
