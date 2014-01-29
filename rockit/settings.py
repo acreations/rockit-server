97339982ac0456704d759ab543a5f06bcfbc955e
acreations@aeris <aaron.wong.home@gmail.com>
2014-01-29 23:44:54 +0100

    Add dj-celery to INSTALLED_APPS

--- rockit/settings.py
+++ rockit/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djcelery',
     'rest_framework',
     'rockit.foundation.core',
     'south'
