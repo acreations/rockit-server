33086e55d974dc14fb258894fb534b06fa42cad4
acreations@aeris <aaron.wong.home@gmail.com>
2014-02-02 00:58:44 +0100

    Added node model

--- rockit/foundation/core/models.py
+++ rockit/foundation/core/models.py
@@ -19,3 +19,16 @@ class NodeCategory(models.Model):
     color = models.CharField(max_length=7, default='#000000')
     date_added    = models.DateTimeField(auto_now_add=True, blank=True)
     date_modified = models.DateTimeField(auto_now=True, blank=True)
+
+class Node(models.Model):
+    '''
+    Describes a node in the rockit network. All devices should be coupled to a Node
+    '''
+    uuid = models.CharField(max_length=36,  blank=True)
+    name = models.CharField(max_length=200, default='undefined')
+    category       = models.ForeignKey(NodeCategory, null=True, blank=True, default=None)
+    association    = models.ForeignKey(Association)
+    aid            = models.CharField(max_length=100, blank=True)
+    description    = models.CharField(max_length=500, blank=True)
+    date_added     = models.DateTimeField(auto_now_add=True, blank=True)
+    date_modified  = models.DateTimeField(auto_now=True, blank=True)
