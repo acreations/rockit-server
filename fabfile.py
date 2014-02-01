cd5fb4efde43f6615c9968d6bc2a829bea91d3bc
acreations@aeris <aaron.wong.home@gmail.com>
2014-02-02 00:43:49 +0100

    Changed from tabs to spaces, pep8

--- fabfile.py
+++ fabfile.py
@@ -2,18 +2,18 @@ from fabric.api import local
 from fabric.api import warn_only
 
 def auto_migrate():
-	with warn_only():
-		local('python manage.py schemamigration rockit.foundation.core --auto')	
+    with warn_only():
+        local('python manage.py schemamigration rockit.foundation.core --auto')
 
 def build():
-	migrate('rockit.foundation.core')
-	test()	
+    migrate('rockit.foundation.core')
+    test()	
 
 def migrate(app):
-	local('python manage.py migrate ' + app)
+    local('python manage.py migrate ' + app)
 
 def setup(environment):
-	local('pip install -r requirements/' + environment)
+    local('pip install -r requirements/' + environment)
 
 def test():
-	local('python manage.py test')
+    local('python manage.py test')
--- rockit/foundation/core/tests/test_association.py
+++ rockit/foundation/core/tests/test_association.py
@@ -4,10 +4,10 @@ from rockit.foundation.core.models import Association
 
 class AssociationTestCase(TestCase):
 
-	def setUp(self):
-		Association.objects.create(name='a', description='rockit description')
+    def setUp(self):
+        Association.objects.create(name='a', description='rockit description')
 
-	def test_it_should_not_be_addable_by_default(self):
-		"""An association should not be addable by default"""
-		association = Association.objects.get(name='a')
-		self.assertEqual(association.addable, False)
+    def test_it_should_not_be_addable_by_default(self):
+        """An association should not be addable by default"""
+        association = Association.objects.get(name='a')
+        self.assertEqual(association.addable, False)
