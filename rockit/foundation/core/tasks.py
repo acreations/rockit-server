0809f5eb1fbc5a8cacfa38e1d815a9da7abbed3b
acreations@aeris <aaron.wong.home@gmail.com>
2014-02-02 02:42:31 +0100

    Added register task and unit tests

---
+++ rockit/foundation/core/tasks.py
@@ -0,0 +1,28 @@
+from celery import task
+from celery.utils.log import get_task_logger
+
+from rockit.foundation.core.models import Node
+
+import uuid
+
+logger = get_task_logger(__name__)
+
+@task(name='rockit-register-node')
+def register(association, aid):
+    logger.debug("Trying to register node (%s) to rockit network" % aid)
+
+    if association and aid:
+        node, created = Node.objects.get_or_create(association=association, aid=aid)
+
+        if created:
+            node.uuid = uuid.uuid4()
+            node.save()
+
+            logger.debug("Node (%s) successfully registered to rockit network" % aid)
+            return True
+        else:
+            logger.warn("Node (%s) has already been created" % aid)
+    else:
+        logger.warn("Assocation or aid cannot be empty")
+
+    return False
---
+++ rockit/foundation/core/tests/test_node.py
@@ -0,0 +1,66 @@
+from django.test import TestCase
+
+from rockit.foundation.core.tasks import register
+
+from rockit.foundation.core.models import Node
+from rockit.foundation.core.models import Association
+
+class NodeTestCase(TestCase):
+
+    def setUp(self):
+        self.association = Association.objects.create(name = 'my_node')
+
+    def test_it_should_not_add_if_association_is_empty(self):
+        aid = 'node_empty_association'
+
+        r = register(None, aid)
+        
+        self.assertEqual(r, False)
+        self.assertEqual(Node.objects.filter(aid=aid).count(), 0)
+
+    def test_it_should_not_add_if_aid_is_empty(self):
+        r = register(self.association, None)
+        
+        self.assertEqual(r, False)
+        self.assertEqual(Node.objects.filter(association=self.association).count(), 0)
+
+    def test_it_should_not_add_if_aid_and_association_is_empty(self):
+        self.assertEqual(register(None, None), False)
+
+
+    def test_it_should_successfully_register_node(self):
+        aid = 'node_success'
+
+        r = register(self.association, aid)
+
+        self.assertEqual(r, True)
+        self.assertEqual(Node.objects.filter(aid=aid).count(), 1)
+
+    def test_it_should_not_register_same_node_twice(self):
+        aid = 'node_success'
+
+        r = register(self.association, aid)
+
+        self.assertEqual(r, True)
+        self.assertEqual(Node.objects.filter(aid=aid).count(), 1)
+
+        r = register(self.association, aid)
+
+        self.assertEqual(r, False)
+        self.assertEqual(Node.objects.filter(aid=aid).count(), 1)
+
+    def test_it_should_generate_unique_uuid(self):
+        size = 10
+
+        for i in range(size):
+            self.assertEqual(register(self.association, 'node_%s' % i), True)
+
+        nodes = Node.objects.all()
+
+        self.assertEqual(nodes.count(), size)
+
+        unique = dict()
+
+        for node in nodes:
+            self.assertEqual(node.uuid in unique, False)
+            unique[node.uuid] = node.uuid
