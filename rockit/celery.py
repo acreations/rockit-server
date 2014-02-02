64dbaa1b2a702f80d078ea34f07a27cdc790b61e
acreations@aeris <aaron.wong.home@gmail.com>
2014-02-02 02:40:42 +0100

    Added amqp for celery

--- rockit/celery.py
+++ rockit/celery.py
@@ -9,7 +9,7 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rockit.settings')
 
-app = Celery('rockit')
+app = Celery('rockit', backend='amqp', broker='amqp://guest@localhost/')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
