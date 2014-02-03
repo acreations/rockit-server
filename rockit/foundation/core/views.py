270299493da0dd5b8cc5e84b024a2eb52db0e5cd
acreations@aeris <aaron.wong.home@gmail.com>
2014-02-03 23:22:31 +0100

    Update inital data and readonly association viewset

--- rockit/foundation/core/views.py
+++ rockit/foundation/core/views.py
@@ -1,14 +1,16 @@
 from rest_framework import viewsets
+from rest_framework.views import APIView
 
 from rockit.foundation.core import models
 from rockit.foundation.core import serializers
 
 from rest_framework.reverse import reverse_lazy
 
-class AssociationViewSet(viewsets.ModelViewSet):
+class AssociationViewSet(viewsets.ReadOnlyModelViewSet):
     """
     API endpoint that allows nodes to be view and set. 
 
     """
     queryset = models.Association.objects.all()
     serializer_class = serializers.AssociationSerializer
+
