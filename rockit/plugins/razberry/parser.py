9fa227b43b148085f05b4464dfcb3f5824e47471
acreations <aaron.wong.home@gmail.com>
2014-02-12 19:13:19 +0100

    Updated parser and model

--- rockit/plugins/razberry/parser.py
+++ rockit/plugins/razberry/parser.py
@@ -6,6 +6,31 @@ logger = logging.getLogger(__name__)
 
 class RazberryParser(object):
 
+    def __init__(self):
+        self._supportedCommandClasses = {
+            '37':  True, # SwitchBinary
+            '39':  True, # SwitchAll
+            '50':  True, # Meter
+            '114': True, # ManufacturerSpecific
+            '134': True, # Version
+            '129': True, # Clock
+            '138': True, # Time
+        }
+        self._ignoredCommandClasses = {
+            '32':  True, # Basic
+            '38':  True, # SwitchMultilevel
+            '34':  True, # ApplicationStatus
+            '43':  True, # SceneActivation
+            '49':  True, # SensorMultilevel
+            '70':  True, # ClimateControlSchedule
+            '96':  True, # MultiChannel
+            '112': True, # Configuration
+            '119': True, # NodeNaming
+            '133': True, # Association
+            '143': True, # MultiCmd
+            '152': True, # Security
+        }
+
     def parseDevices(self, devices):
         if devices:
             for key,device in devices.items():
@@ -13,7 +38,6 @@ class RazberryParser(object):
                 node, created = models.Node.objects.get_or_create(device_id=key)
 
                 node.device_type = self._getValue(data['deviceTypeString'])
-                node.device_name = self._getValue(data[''])
 
                 node.manufacturer_id   = self._getValue(data['manufacturerId'])
                 node.manufacturer_name = self._getValue(data['vendorString'])
@@ -23,11 +47,9 @@ class RazberryParser(object):
                 node.awaken = self._getValue(data['isAwake'])
                 node.failed = self._getValue(data['isFailed'])
 
-    frequent_listening = models.BooleanField(default = False)
                 node.beaming = self._getValue(data['beam'])
-    security = models.BooleanField(default = False)
 
-                print self._getValue(data['isRouting'])
+                self._parseInstances(node, device['instances'])
 
             return len(devices.keys())
 
@@ -37,3 +59,25 @@ class RazberryParser(object):
         if data and data['value']:
             return data['value']
         return defaultValue
+
+    def _parseCommandClasses(self, node, commandClasses):
+        if node and commandClasses:            
+            for key in commandClasses:
+                unknown = list()
+
+                if key in self._supportedCommandClasses:
+                    logger.debug(commandClasses[key])
+                elif key in self._ignoredCommandClasses:
+                    pass
+                else:
+                    unknown.append(key)
+        else:
+            logger.warn("Cannot parse command classes, node or command classes are empty")
+
+
+    def _parseInstances(self, node, instances):
+        if node and instances:
+            for key, instance in instances.items():
+                self._parseCommandClasses(node, instance['commandClasses'])
+        else:
+            logger.warn("Cannot parse instances, node or instances are empty")
