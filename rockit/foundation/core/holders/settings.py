02238972d55f4b489ec26c529425149c7a1ccbaa
acreations <aaron.wong.home@gmail.com>
2014-02-13 11:15:53 +0100

    Changed method name

--- rockit/foundation/core/holders/settings.py
+++ rockit/foundation/core/holders/settings.py
@@ -1,11 +1,17 @@
 from rockit.foundation.core.holders.holder import Holder
 
 class SettingsHolder(Holder):
+	"""Settings holder is used to help plugins to collect settings."""
 
-    def add_simple(self, key, name, value, readonly=False):
-        self.append({
-            'id':    key,
-            'name':  name,
-            'value': value,
-            'isReadonly': readonly
-            })
+    def add_setting(self, key, name, value, readonly=False):
+    	"""
+    	Add a simple settings
+
+    	Keyword arguments:
+    	key      -- unique key
+    	name     -- name 
+    	value    -- current value
+    	readonly -- specifies if this is readonly
+
+    	"""
+        self.append({'id': key, 'name':  name, 'value': value, 'isReadonly': readonly})
