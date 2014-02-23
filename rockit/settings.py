6ae9537362e2390cc186a4d9169680db82431a38
acreations@aeris <aaron.wong.home@gmail.com>
2014-02-23 22:27:27 +0100

    Activated middleware class for cors

--- rockit/settings.py
+++ rockit/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',    
 )
 
 ROOT_URLCONF = 'rockit.urls'
