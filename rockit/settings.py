f9d4a3ebe2e96b34595ea068708bdf18e1ee75ed
acreations@aeris <aaron.wong.home@gmail.com>
2014-03-28 22:42:09 +0100

    Added static root

--- rockit/settings.py
+++ rockit/settings.py
@@ -88,7 +88,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles/')
+STATIC_URL  = '/static/'
 
 LOGGING = {
     'version': 1,
