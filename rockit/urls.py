ba8f025b4cfb80a4db300cf5560118ed854318eb
acreations@aeris <aaron.wong.home@gmail.com>
2014-04-21 15:38:23 +0200

    Updated urls

--- rockit/urls.py
+++ rockit/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from rockit.foundation.core import models
+from rockit import settings
 
 urlpatterns = list()
 
@@ -14,3 +15,9 @@ for a in models.Association.objects.all():
         urlpatterns += patterns('', url(r'^%s%s/' % (prefix,a.entry), include(a.namespace + '.urls')))
     except ImportError:
         continue
+
+#for app in settings.INSTALLED_APPS:
+#    if app.startswith('rockit'):
+#        entry = 'rockit' if 'plugins' not in app else app.split('.')[-1]
+#        urls  = '%s.urls' % app
+#        urlpatterns += patterns('', url(r'^%s/' % entry, include(app)))
