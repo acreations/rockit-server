4ebc725e68c235726342c6ac7a3ccaaee7ad3428
acreations@aeris <aaron.wong.home@gmail.com>
2014-04-21 22:55:20 +0200

    Changed master urls to use INSTALLED_APPS instead of db

---
+++ rockit/plugins/razberry/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+
+from rest_framework import routers
+
+from rockit.plugins.mailout import views
+
+router = routers.DefaultRouter()
+#router.register('nodes',   views.NodeViewSet)
+#router.register('servers', views.ServerViewSet)
+
+urlpatterns = patterns('abc',
+    url('', include(router.urls))
+)
--- rockit/urls.py
+++ rockit/urls.py
@@ -5,19 +5,11 @@ from rockit import settings
 
 urlpatterns = list()
 
-for a in models.Association.objects.all():
-    try:
-        prefix = ''
-
-        if('plugins' in a.namespace):
-            prefix = 'plugins/'
-
-        urlpatterns += patterns('', url(r'^%s%s/' % (prefix,a.entry), include(a.namespace + '.urls')))
-    except ImportError:
-        continue
-
-#for app in settings.INSTALLED_APPS:
-#    if app.startswith('rockit'):
-#        entry = 'rockit' if 'plugins' not in app else app.split('.')[-1]
-#        urls  = '%s.urls' % app
-#        urlpatterns += patterns('', url(r'^%s/' % entry, include(app)))
+for app in settings.INSTALLED_APPS:
+    if app.startswith('rockit'):
+        '''
+        Get all rockit plugins defined in INSTALLED_APPS
+        '''
+        entry = 'rockit' if 'plugins' not in app else 'rockit/plugins/%s' % app.split('.')[-1]
+        
+        urlpatterns += patterns('', url(r'^%s/' % entry, include('%s.urls' % app)))
