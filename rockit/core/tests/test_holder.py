2377fa7faa07e9a6e532513e3946504c1b716338
acreations@aeris <aaron.wong.home@gmail.com>
2014-04-24 00:01:58 +0200

    Added unit test for holder

--- rockit/core/tests/test_holder.py
+++ rockit/core/tests/test_holder.py
@@ -7,3 +7,17 @@ class HolderTestCase(TestCase):
     def setUp(self):
         self.holder = Holder()
 
+    def test_it_should_be_able_to_append(self):
+        self.holder.append('TEST-01')
+        self.holder.append('TEST-02')
+        self.holder.append('TEST-03')
+        self.holder.append('TEST-04')
+        self.holder.append('TEST-05')
+
+        content = self.holder.get_content()
+
+        self.assertEqual(True, 'TEST-01' in str(content))
+        self.assertEqual(True, 'TEST-02' in str(content))
+        self.assertEqual(True, 'TEST-03' in str(content))
+        self.assertEqual(True, 'TEST-04' in str(content))
+        self.assertEqual(True, 'TEST-05' in str(content))
