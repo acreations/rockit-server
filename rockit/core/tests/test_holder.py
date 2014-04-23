f2041d71948a14d71a72f4f59caa5aaa718d89b5
acreations@aeris <aaron.wong.home@gmail.com>
2014-04-24 00:15:28 +0200

    Added more unit test to holder

--- rockit/core/tests/test_holder.py
+++ rockit/core/tests/test_holder.py
@@ -9,15 +9,20 @@ class HolderTestCase(TestCase):
 
     def test_it_should_be_able_to_append(self):
         self.holder.append('TEST-01')
+        self.assertEqual(True, 'TEST-01' in str(self.holder.get_content()))
+
+    def test_content_should_be_a_dict(self):
+        self.assertTrue(isinstance(self.holder.get_content(), dict))
+
+    def test_content_should_contain_data(self):
+        self.assertTrue('data' in self.holder.get_content())
+
+    def test_data_should_contain_same_as_appended(self):
+        self.holder.append('TEST-01')
         self.holder.append('TEST-02')
-        self.holder.append('TEST-03')
-        self.holder.append('TEST-04')
-        self.holder.append('TEST-05')
 
-        content = self.holder.get_content()
+        data = self.holder.get_content()['data']
 
-        self.assertEqual(True, 'TEST-01' in str(content))
-        self.assertEqual(True, 'TEST-02' in str(content))
-        self.assertEqual(True, 'TEST-03' in str(content))
-        self.assertEqual(True, 'TEST-04' in str(content))
-        self.assertEqual(True, 'TEST-05' in str(content))
+        self.assertEqual(2, len(data))
+        self.assertEqual('TEST-01', data[0])
+        self.assertEqual('TEST-02', data[1])
