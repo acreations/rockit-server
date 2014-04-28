9af4ced0523f8ba6fbc8a8b0da8a3a315bb1dcdf
acreations@aeris <aaron.wong.home@gmail.com>
2014-04-28 21:34:01 +0200

    Added unit test for commands holder

--- rockit/core/tests/test_holder_commands.py
+++ rockit/core/tests/test_holder_commands.py
@@ -7,7 +7,7 @@ class CommandsHolderTestCase(TestCase):
     def setUp(self):
         self.holder = CommandsHolder()
 
-    def test_it_should_be_able_to_add(self):
+    def test_it_should_be_able_to_add_switch_command(self):
         self.holder.add_switch_command('IDENTIFIER', 'TEST_BINARY', 'true', 'false', 'false')
 
         content = self.holder.get_content()
@@ -17,3 +17,13 @@ class CommandsHolderTestCase(TestCase):
         self.assertEqual(True, 'on' in str(content))
         self.assertEqual(True, 'off' in str(content))
         self.assertEqual(True, 'current' in str(content))
+
+    def test_it_should_be_able_to_add_choice_command(self):
+        self.holder.add_choice_command('TEST_CHOICE')
+
+        # Add more unit test later
+
+    def test_it_should_be_able_to_add_scales_command(self):
+        self.holder.add_scales_command('TEST_SCALES')
+
+        # Add more unit test later
