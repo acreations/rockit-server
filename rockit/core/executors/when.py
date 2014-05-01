a368c71f23be65c8051c575d7371fd7e7c504386
acreations@aeris <aaron.wong.home@gmail.com>
2014-05-01 23:26:07 +0200

    Added when executor

---
+++ rockit/core/executors/when.py
@@ -0,0 +1,17 @@
+
+class WhenExecutor(object):
+
+    def __init__(self):
+
+        self.capabilities = []
+
+    def collect(holder):
+        '''
+        Collect capabilities of this executor
+        '''
+        return holder
+
+    def _add_capabilities(identifier, name):
+        self.capabilities.push({
+            'identifier'
+            })
