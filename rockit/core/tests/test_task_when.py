954b389af37121fa915dda2525a2b586fd9fc736
acreations@aeris <aaron.wong.home@gmail.com>
2014-05-03 22:59:43 +0200

    Fixed unit test

--- rockit/core/tests/test_task_when.py
+++ rockit/core/tests/test_task_when.py
@@ -1,12 +1,13 @@
 from django.test import TestCase
 
 from rockit.core import holders
+from rockit.core import models
 from rockit.core import tasks
 
-class TaskSettingsTestCase(TestCase):
+class TaskWhenTestCase(TestCase):
 
     def test_it_should_be_able_to_call_task(self):
-        holder = holders.WhenHolder()
+        holder = holders.WhenHolder(models.Association.objects.create(name = 'my_node', namespace='test'))
         holder = tasks.when(holder)
 
         self.assertNotEqual(0, len(holder.get_content()))
