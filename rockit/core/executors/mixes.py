f55d49c80cf6fb45977a745c87644daeb3285821
acreations@aeris <aaron.wong.home@gmail.com>
2014-05-04 21:51:45 +0200

    Refactoring

--- rockit/core/executors/mixes.py
+++ rockit/core/executors/mixes.py
@@ -4,26 +4,25 @@ class MixesExecutor(object):
     IDENTIFIER_BUTTON   = 1001;
     IDENTIFIER_SCHEDULE = 1002;
 
-    when_capabilities = []
-
-    def __init__(self):
-
-        self._add_when_capabilities(self.IDENTIFIER_BUTTON, 'button')
-        self._add_when_capabilities(self.IDENTIFIER_SCHEDULE, 'schedule')
-
     def collect(self, holder):
         '''
         Collect capabilities of this executor
         '''
-        holder.reset()
-
-        for c in self.when_capabilities:
+        for c in self._get_when_capabilities():
             holder.add_when(**c)
 
         return holder
 
-    def _add_when_capabilities(self, identifier, name):
-        self.when_capabilities.append({
+    def _add_when_capabilities(self, container, identifier, name):
+        container.append({
             'identifier': identifier,
             'name': name
             })
+
+    def _get_when_capabilities(self):
+        result = list()
+
+        self._add_when_capabilities(result, self.IDENTIFIER_BUTTON, 'button')
+        self._add_when_capabilities(result, self.IDENTIFIER_SCHEDULE, 'schedule')
+
+        return result
