acf85265fa432bd8193fdb4aeeb347689bf10804
acreations@aeris <aaron.wong.home@gmail.com>
2014-05-07 00:32:02 +0200

    Added test for task details (rockit)

--- rockit/core/tests/test_task_mixes.py
+++ rockit/core/tests/test_task_mixes.py
@@ -4,10 +4,16 @@ from rockit.core import holders
 from rockit.core import models
 from rockit.core import tasks
 
-class TaskWhenTestCase(TestCase):
+class TaskMixesTestCase(TestCase):
 
     def test_it_should_be_able_to_call_task(self):
         holder = holders.MixesHolder(models.Association.objects.create(name = 'my_node', namespace='test'))
         holder = tasks.mixes(holder)
 
         self.assertNotEqual(0, len(holder.get_content()))
+
+    def test_it_should_be_able_to_call_mixes_detail_task(self):
+        holder = holders.MixesDetailsHolder()
+        holder = tasks.mixes_details('when-button', holder)
+
+        self.assertTrue('POST' in holder.get_content()['actions'])
