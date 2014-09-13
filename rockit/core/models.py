f0666ddfeee48325fb776d8c4b03b7476102f35b
acreations@aeris <aaron.wong.home@gmail.com>
2014-09-13 12:01:31 +0200

    Updated model

--- rockit/core/models.py
+++ rockit/core/models.py
@@ -67,3 +67,23 @@ class Setting(models.Model):
     readonly    = models.BooleanField(default=True)
     date_added     = models.DateTimeField(auto_now_add=True, blank=True)
     date_modified  = models.DateTimeField(auto_now=True, blank=True)
+
+class UserRole(models.model):
+    """
+    Create a role based user model
+    """
+    name = models.CharField(max_length=50)
+    weight = models.IntegerField()
+    date_added = models.DateTimeField(auto_now_add=True, blank=True)
+    date_modified  = models.DateTimeField(auto_now=True, blank=True)
+
+class User(models.model):
+    """
+    Defines a user that can be used with rockit server
+    """
+    openid = models.CharField(max_length=36, blank=True)
+    name = models.CharField(max_length=50)
+    mail = models.CharField(max_length=100)
+    role = models.CharField(max_length=50)
+    date_added = models.DateTimeField(auto_now_add=True, blank=True)
+    date_modified  = models.DateTimeField(auto_now=True, blank=True)
