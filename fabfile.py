9d6e0dc23be9e6cbffc13cb09d02707347ea121c
acreations@aeris <aaron.wong.home@gmail.com>
2014-12-12 22:44:53 +0100

    Updated fabfile

--- fabfile.py
+++ fabfile.py
@@ -2,18 +2,9 @@ from fabric.api import local
 
 CMD_MANAGE = "python manage.py "
 
-def auto_schema():
-    schema('rockit.core')
-    #schema('rockit.plugins.mailout')
-    schema('rockit.plugins.razberry')
-
 def build():
-    migrate('rockit.core')
-    migrate('rockit.plugins.mailout')
-    migrate('rockit.plugins.razberry')
-    
-    load_data('rockit/foundation/core/fixtures/settings.json')
-    load_data('rockit/plugins/mailout/fixtures/servers.json')
+    local(CMD_MANAGE + 'makemigrations')
+    local(CMD_MANAGE + 'migrate')
 
     test()
 
@@ -23,17 +14,11 @@ def celery():
 def load_data(path):
     local(CMD_MANAGE + 'loaddata %s' % path)
 
-def migrate(app):
-    local(CMD_MANAGE + 'migrate %s' % app)
-
 def runserver(localonly=True):
     if localonly:
         local(CMD_MANAGE + 'runserver')
     else:
         local(CMD_MANAGE + 'runserver 0.0.0.0')
-
-def schema(app):
-    local(CMD_MANAGE + 'schemamigration %s --auto' % app)
 
 def setup(environment):
     local('pip install -r requirements/%s' % environment)
