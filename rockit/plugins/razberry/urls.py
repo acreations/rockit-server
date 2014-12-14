433689c21b211de656a06359c87bd6729079053d
acreations@aeris <aaron.wong.home@gmail.com>
2014-12-14 18:05:44 +0100

    Removed unused files

--- rockit/plugins/razberry/urls.py
+++ rockit/plugins/razberry/urls.py
@@ -2,12 +2,8 @@ from django.conf.urls import patterns, include, url
 
 from rest_framework import routers
 
-from rockit.plugins.mailout import views
-
 router = routers.DefaultRouter()
-#router.register('nodes',   views.NodeViewSet)
-#router.register('servers', views.ServerViewSet)
 
-urlpatterns = patterns('abc',
+urlpatterns = patterns('',
     url('', include(router.urls))
 )
