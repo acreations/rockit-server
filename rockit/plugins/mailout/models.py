7aaee8f5337a5d63dbd9093660ee9d388983e1ff
acreations@aeris <aaron.wong.home@gmail.com>
2014-12-14 18:01:15 +0100

    Removed models for mailout

--- rockit/plugins/mailout/models.py
+++ rockit/plugins/mailout/models.py
@@ -1,22 +1,5 @@
 from django.db import models
 
-class Server(models.Model):
-    """
-    Server configuration for mail
-    """
-    name = models.CharField(max_length=200, default='undefined')
-    description  = models.CharField(max_length=500, blank=True)
-    date_added    = models.DateTimeField(auto_now_add=True, blank=True)
-    date_modified = models.DateTimeField(auto_now=True, blank=True)
-
-class Node(models.Model):
-    """
-    Mailout Node
-    """
-    server = models.ForeignKey(Server)
-    date_added     = models.DateTimeField(auto_now_add=True, blank=True)
-    date_modified  = models.DateTimeField(auto_now=True, blank=True)
-
 class Setting(models.Model):
     """
     Settings
