bfa1a0b355de0f2f40f37730e46beef98601ea0d
acreations@aeris <aaron.wong.home@gmail.com>
2014-12-14 19:22:24 +0100

    Fixed razberry

--- rockit/plugins/razberry/migrations/0001_initial.py
+++ rockit/plugins/razberry/migrations/0001_initial.py
@@ -3,76 +3,24 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
+def register_to_rockit(apps, schema_editor):
+
+    Association = apps.get_model("core", "Association")
+
+    association = Association()
+    association.name        = "Razberry"
+    association.description = "Communicate with Z-Wave devices using Razberry"
+    association.namespace   = "rockit.plugins.razberry"
+    association.entry       = "razberry"
+    association.addable     = True
+    association.save()
 
 class Migration(migrations.Migration):
 
     dependencies = [
+      ('core', '0001_initial'),
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Node',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('device_id', models.CharField(max_length=100)),
-                ('device_type', models.CharField(default=b'', max_length=50)),
-                ('manufacturer_id', models.CharField(default=b'', max_length=50)),
-                ('manufacturer_name', models.CharField(default=b'', max_length=50)),
-                ('version', models.CharField(default=b'', max_length=10)),
-                ('listening', models.BooleanField(default=False)),
-                ('routing', models.BooleanField(default=False)),
-                ('beaming', models.BooleanField(default=False)),
-                ('awaken', models.BooleanField(default=False)),
-                ('failed', models.BooleanField(default=False)),
-                ('date_added', models.DateTimeField(auto_now_add=True)),
-                ('date_modified', models.DateTimeField(auto_now=True)),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='NodeCommand',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=50)),
-                ('identifier', models.IntegerField()),
-                ('namespace', models.CharField(default=b'', max_length=100)),
-                ('date_added', models.DateTimeField(auto_now_add=True)),
-                ('date_modified', models.DateTimeField(auto_now=True)),
-                ('node', models.ForeignKey(to='razberry.Node')),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='NodeVersion',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('sdk', models.CharField(default=b'', max_length=16)),
-                ('application', models.CharField(default=b'', max_length=16)),
-                ('zw_library', models.IntegerField(default=0)),
-                ('zw_protocol', models.CharField(default=b'', max_length=16)),
-                ('node', models.ForeignKey(to='razberry.Node')),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='Setting',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=50)),
-                ('description', models.CharField(max_length=500, blank=True)),
-                ('value', models.CharField(max_length=100, blank=True)),
-                ('readonly', models.BooleanField(default=True)),
-                ('date_added', models.DateTimeField(auto_now_add=True)),
-                ('date_modified', models.DateTimeField(auto_now=True)),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
+      migrations.RunPython(register_to_rockit),
     ]
--- rockit/plugins/razberry/models.py
+++ rockit/plugins/razberry/models.py
@@ -54,7 +54,6 @@ class Setting(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=500, blank=True)
     value       = models.CharField(max_length=100, blank=True)
-    default     = models.CharField(max_length=100)
-    readonly    = models.BooleanField(default=True)
+    readonly    = models.BooleanField(default=False)
     date_added     = models.DateTimeField(auto_now_add=True, blank=True)
     date_modified  = models.DateTimeField(auto_now=True, blank=True)
