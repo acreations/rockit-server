4b9770c55aa2b9e8449f59adcd9820fce3c327e3
acreations@aeris <aaron.wong.home@gmail.com>
2015-01-05 16:38:33 +0100

    Added next date for scheduler

--- rockit/core/management/commands/rockitscheduler.py
+++ rockit/core/management/commands/rockitscheduler.py
@@ -18,39 +18,36 @@ class Command(BaseCommand):
     help = 'Run action based on current time'
 
     logger = logging.getLogger(__name__)
-    tmzone = pytz.timezone(settings.TIME_ZONE)
 
     def handle(self, *args, **options):
         '''
         Handle it
         '''
 
-        schedules = models.Schedule.objects.all()
+        tmzone = pytz.timezone(settings.TIME_ZONE)
+        schedules = models.Schedule.objects.filter(date_next__lte=self.tmzone.localize(datetime.now()))
 
         for schedule in schedules:
 
-            if self.has_triggered_schedule(schedule):
-                self.logger.debug('Run action for this schedule')
+            self.logger.info('Run action for this schedule')
 
-                node = self.get_node("be174f43-c616-4b31-a64d-8beea39688bc")
+            node = self.get_node("be174f43-c616-4b31-a64d-8beea39688bc")
 
-                if node:
+            if node:
 
-                    update = send_task("%s.node.command.update.value" % node.association.entry, kwargs={
-                        'identifier': node.aid,
-                        'command_id': "devices.3.instances.0.commandClasses.37",
-                        'value': self.normalize_value("true")
-                        })
-                    value = update.wait(timeout=30)
+                update = send_task("%s.node.command.update.value" % node.association.entry, kwargs={
+                    'identifier': node.aid,
+                    'command_id': "devices.3.instances.0.commandClasses.37",
+                    'value': self.normalize_value("true")
+                    })
+                value = update.wait(timeout=30)
 
-                    if value is not None:
-                        self.logger.debug('Successful update value ... saving')
+                if value is not None:
+                    self.logger.debug('Successful update value ... saving')
 
-                        # schedule.save()
-                    else:
-                        self.logger.warn('Failed to set value of node %s' % node.uuid)
-            else:
-                self.logger.info('Schedule has not been triggered yet')
+                    # schedule.save()
+                else:
+                    self.logger.warn('Failed to set value of node %s' % node.uuid)
 
     def get_node(self, uuid):
         try:
@@ -58,17 +55,6 @@ class Command(BaseCommand):
         except:
             self.logger.error('Could not find node with uuid %s' % uuid)
         return None
-
-    def has_triggered_schedule(self, schedule):
-        '''
-        Check if schedule has beed triggered based on cron setting
-        '''
-
-        cron = croniter(schedule.cron, schedule.date_modified)
-        next = cron.get_next(datetime)
-        now  = self.tmzone.localize(datetime.now())
-
-        return now > next
 
     def normalize_value(self, value):
         if isinstance(value, bool):
