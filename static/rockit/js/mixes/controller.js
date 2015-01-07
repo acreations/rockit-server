30b71ee083327afca25aa4e2208ef0a1aec05f53
acreations@aeris <aaron.wong.home@gmail.com>
2015-01-08 00:15:44 +0100

    Updated mixs

--- static/rockit/js/mixes/controller.js
+++ static/rockit/js/mixes/controller.js
@@ -1,62 +1,80 @@
 define(['jquery'], function ($) {
   'use strict';
 
-  return ['$scope', '$log', 'MixesService', function (scope, log, service) {
+  return ['$scope', '$log', '$timeout', 'MixesService', function (scope, log, timeout, service) {
 
     scope.criteria = {};
 
-    var onCreate = function () {
-      scope.getCriterias();
-    };
-
-    scope.onSelectWhen = function (item) {
-      var identifier = item.identifier;
-
-      log.debug('Trying to get detailed info about', identifier);
-      service.get(item.url).then(
-        function (data) {
-          log.debug('Successfully retrieved detailed info', data);
-
-          scope.criteria.when = data;
-
-          console.log(scope.criteria);
-        },
-        function () {
-          log.error('Exception when trying to get detailed info about', identifier);
-        }
-      );
-    };
-
-    scope.toggle = function (container, item, anchorID) {
-      // Reset criteria for selected container/group
-      scope.criteria[container.id] = false;
-
-      // Either select it or toggle it
-      container.selected = container.selected === item ? null : item;
-    };
-
     scope.getCriterias = function () {
+
+      var createContainer = function (id, groups) {
+        return {
+          id: id,
+          groups: groups,
+          selection: {},
+          loading: {}
+        };
+      };
+
+      scope.loading = true;
       service.list().then(
         function (data) {
           log.debug('Successful retrieved mixes criteria', data);
 
-          scope.when = {
-            id: 'when',
-            data: data.when
-          };
+          scope.when = createContainer('when', data.when);
+          scope.then = createContainer('then', data.then);
+          scope.finish = createContainer('finsih', data.finish);
         },
         function () {
           log.error('Exception when trying to get associations');
         }
-      );
+      ).finally(function () {
+        scope.loading = false;
+      });
+    };
+
+    scope.onSelectCriteria = function (container, item, anchorID) {
+      container.selection.criteria = item;
+
+      log.debug('Trying to get detailed info about', container.selection.criteria.identifier);
+
+      container.loading.criteria = true;
+      service.get(item.url).then(
+        function (data) {
+          log.debug('Successfully retrieved detailed info', data);
+
+          container.criteria = data;
+
+          console.log(container);
+
+          scope.scrollTo(anchorID);
+        },
+        function () {
+          log.error('Exception when trying to get detailed info about', container.selection.criteria.identifier);
+        }
+      ).finally(function () {
+        container.loading.criteria = false;
+      });
+    };
+
+    scope.toggleGroup = function (container, item, anchorID) {
+      // Reset criteria for selected container/group
+      scope.criteria[container.id] = false;
+
+      // Either select it or toggle it
+      container.selection.group = container.selection.group === item ? null : item;
+
+      // Scroll to anchor
+      scope.scrollTo(anchorID);
     };
 
     scope.scrollTo = function (id) {
-      $('html, body').animate({
-        scrollTop: $(id).offset().top
-      }, 1000);
-    }
+      timeout(function () {
+        $('html, body').animate({
+          scrollTop: $(id).offset().top
+        }, 1000);
+      }, 0);
+    };
 
-    onCreate();
   }];
 });
