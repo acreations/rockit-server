aa32305f66f52d02546e0257081e2a7a4b49734e
acreations@aeris <aaron.wong.home@gmail.com>
2015-01-17 12:32:29 +0100

    Updated mix criterias

--- static/rockit/js/directives/input/clockpicker.js
+++ static/rockit/js/directives/input/clockpicker.js
@@ -10,13 +10,22 @@ define(['jquery', 'clockpicker'], function ($) {
       templateUrl: '/static/rockit/js/directives/input/clockpicker.template',
       link: function (scope, element) {
 
+        var lastSetValue = '';
+
+        scope.$watch('model', function () {
+          if (lastSetValue) {
+            scope.model = lastSetValue;
+          }
+        });
+
         var clockpicker = $(element).clockpicker({
           'default': 'now',
-          donetext: 'Set',
+          donetext: 'Done',
           afterDone: function () {
+            lastSetValue = clockpicker.find("#clockpicker-value").val();
             // Set added value
             scope.$apply(function () {
-              scope.model = clockpicker.find("#clockpicker-value").val();
+              scope.model = lastSetValue;
             });
           }
         });
