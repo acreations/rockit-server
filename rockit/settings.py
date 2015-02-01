ecceb594a2a4484c01982ca725a751b6a0865fb3
acreations@aeris <aaron.wong.home@gmail.com>
2015-02-01 22:18:15 +0100

    Refactored settings

--- rockit/settings.py
+++ rockit/settings.py
@@ -1,154 +1,45 @@
-"""
-Django settings for rockit project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import datetime
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+## Import our defaults (globals)
 
-DB_NAME = "rockit.sqlite3"
+from rockit.conf.settings.default import *
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+## Inherit from environment specifics
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e04#=m)0nv#^$0z+m#cf2+bm#4y^6se_e%!fm(k+_1#&wiqm2y'
+DJANGO_CONF = os.environ.get('DJANGO_CONF', 'default')
+if DJANGO_CONF != 'default':
+    module = __import__(DJANGO_CONF, globals(), locals(), ['*'])
+    for k in dir(module):
+        locals()[k] = getattr(module, k)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+## Import local settings
 
-TEMPLATE_DEBUG = True
+try:
+    from local_settings import *
+except ImportError:
+    import sys, traceback
+    sys.stderr.write("Warning: Can't find the file 'local_settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+    sys.stderr.write("\nFor debugging purposes, the exception was:\n\n")
+    traceback.print_exc()
 
-ALLOWED_HOSTS = []
+## Remove disabled apps
 
-# Application definition
+if 'DISABLED_APPS' in locals():
+    INSTALLED_APPS = [k for k in INSTALLED_APPS if k not in DISABLED_APPS]
 
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'djcelery',
-    'django_jenkins',
-    'djangobower',
-    'corsheaders',
-    'rest_framework',
-    'compressor',
-    'rockit.core',
-    'rockit.plugins.mailout',
-    'rockit.plugins.picamera',
-    'rockit.plugins.razberry',
-    'rockit.plugins.astral',
-)
+    MIDDLEWARE_CLASSES = list(MIDDLEWARE_CLASSES)
+    DATABASE_ROUTERS = list(DATABASE_ROUTERS)
+    TEMPLATE_CONTEXT_PROCESSORS = list(TEMPLATE_CONTEXT_PROCESSORS)
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.middleware.transaction.TransactionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-)
+    for a in DISABLED_APPS:
+        for x, m in enumerate(MIDDLEWARE_CLASSES):
+            if m.startswith(a):
+                MIDDLEWARE_CLASSES.pop(x)
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.request',
-)
+        for x, m in enumerate(TEMPLATE_CONTEXT_PROCESSORS):
+            if m.startswith(a):
+                TEMPLATE_CONTEXT_PROCESSORS.pop(x)
 
-COMPRESS_PRECOMPILERS = (
-   ('text/less', 'lessc {infile} {outfile}'),
-)
-
-INTERNAL_IPS = ('127.0.0.1',)
-
-ROOT_URLCONF = 'rockit.urls'
-
-WSGI_APPLICATION = 'rockit.wsgi.application'
-
-CORS_ORIGIN_ALLOW_ALL = True
-#CORS_ORIGIN_WHITELIST = ('localhost:9000')
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, DB_NAME),
-    }
-}
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
-STATIC_URL  = '/static/'
-
-STATICFILES_FINDERS = (
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-    "compressor.finders.CompressorFinder",
-)
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "bower_components"),
-)
-
-COMPRESS_ROOT = 'static/'
-
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, 'templates'),
-)
-
-JENKINS_TASKS = ('django_jenkins.tasks.django_tests',)
-TEST_RUNNER = 'djcelery.contrib.test_runner.CeleryTestSuiteRunner'
-PROJECT_APPS = [appname for appname in INSTALLED_APPS if appname.startswith('rockit')]
-
-LOGGING = {
-    'version': 1,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'console':{
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        },
-    },
-    'loggers': {
-        'rockit': {
-            'handlers': ['console'],
-            'level': 'INFO'
-        }
-    }
-}
+        for x, m in enumerate(DATABASE_ROUTERS):
+            if m.startswith(a):
+                DATABASE_ROUTERS.pop(x)
