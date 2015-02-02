5c0934f222c65eb4c7ee331af190d1554abc6400
acreations@aeris <aaron.wong.home@gmail.com>
2015-02-02 20:49:39 +0100

    Updated bower

--- assets/rockit/js/configs/constants.js
+++ assets/rockit/js/configs/constants.js
@@ -2,7 +2,7 @@ define(function () {
   'use strict';
 
   return {
-    'serverUrl': '/api/rockit',
+    'serverUrl': 'http://zack/api/rockit',
     'mockEnabled': false,
     'mockUrl': '//localhost:8000/static/rockit/js'
   };
