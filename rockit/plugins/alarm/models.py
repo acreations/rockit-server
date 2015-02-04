89ab3466f19b60dec4eba83b1c2c3bcfb70de349
acreations@aeris <aaron.wong.home@gmail.com>
2015-02-04 23:30:34 +0100

    Migrate

--- rockit/plugins/alarm/models.py
+++ rockit/plugins/alarm/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+class Alarm(models.Model):
+    """
+    Defines a time to tigger events/actions
+    """
+    cron   = models.CharField(max_length=50, blank=True)
+    repeat = models.BooleanField(default=False)
+    date_next      = models.DateTimeField(blank=True)
+    date_modified  = models.DateTimeField(auto_now=True, blank=True)
+    date_added     = models.DateTimeField(auto_now_add=True, blank=True)
